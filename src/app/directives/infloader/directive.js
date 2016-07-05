9b953416f60546d5e924c46fe740cc0cac362f3a
Alexandr Lapin <xanderwot@gmail.com>
2016-07-06 01:53:44 +0300

    Improve infscroll directive New layout for products list

--- src/app/directives/infloader/directive.js
+++ src/app/directives/infloader/directive.js
@@ -15,17 +15,18 @@ class InfloaderDirective {
     };
   }
 
-  // controller($scope, $state, MessagesService) {
-  //   $scope.state = $state;
-  //   $scope.service = MessagesService;
-  // }
-
   link(scope, element, attrs) {
     let container = angular.element(document.querySelector(`.${scope.containerClass}`));
     container.on('scroll', () => {
       let top = element[0].getBoundingClientRect().top;
       let containerBottom = container[0].getBoundingClientRect().bottom;
       if ((top - 1) < containerBottom && scope.page < scope.total) {
+        scope.currentPage = scope.page + 1;
+        scope.$digest();
+      }
+    });
+    scope.$watch('currentPage', (new_val, old_val) => {
+      if (new_val != old_val) {
         if (scope.searchQuery) {
           scope.loadFunc({q: scope.searchQuery, page: scope.page + 1});
         } else {
